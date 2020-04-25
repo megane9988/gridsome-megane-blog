913e385c5b197fef595352e35d7c3c33b0258648
hajime OGUSHI <info@m-g-n.me>
2020-04-25 19:28:40 +0900

    全体をnoindex , nofollowに変更

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue) {
+export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
+
+  // Add a meta tag
+  head.meta.push({
+    name: 'robots',
+    content: 'noindex , nofollow'
+  })
 }
 
 import '~/assets/styles.scss'
+
